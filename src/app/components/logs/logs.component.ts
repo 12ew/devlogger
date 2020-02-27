9ce90423a793d475d59f958425005b6de84e6300
Abdoul <committed1@gmail.com>
2020-02-27 17:09:17 -0500

    add logs and display dynamically

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.scss']
+  selector: "app-logs",
+  templateUrl: "./logs.component.html",
+  styleUrls: ["./logs.component.scss"]
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string;
+    text: string;
+    date: any;
+  }[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.logs = [
+      {
+        id: "1",
+        text: "Generated components",
+        date: new Date("12/27/2017 12:54:23")
+      },
+      {
+        id: "2",
+        text: "Added login",
+        date: new Date("12/27/2017 1:04:38")
+      },
+      {
+        id: "3",
+        text: "Added bootstrap",
+        date: new Date("12/27/2017 1:34:12")
+      }
+    ];
   }
-
 }
