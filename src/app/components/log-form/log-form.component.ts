4e013e8a9b5c1cbc0dab61d6490554667d079256
Abdoul <committed1@gmail.com>
2020-03-02 11:34:10 -0500

    add update and delete functionality

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -11,6 +11,9 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+
+  isNew: boolean = true;
+
   // inject service into the constructor
   constructor(private logService: LogService) {}
 
@@ -18,10 +21,42 @@ export class LogFormComponent implements OnInit {
     // Subscribe to the selctedLog observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.id;
       }
+    });
+  }
+
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      // Create a new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      };
+      // Add log
+      this.logService.addLog(newLog);
+    } else {
+      //Create log to be uodated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // Update log
+      this.logService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
     });
   }
 }
--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -21,4 +21,10 @@ export class LogsComponent implements OnInit {
   onSelect(log: Log) {
     this.logService.setFormLog(log);
   }
+
+  onDelete(log: Log) {
+    if (confirm("Are yiou sure?")) {
+      this.logService.deleteLog(log);
+    }
+  }
 }
